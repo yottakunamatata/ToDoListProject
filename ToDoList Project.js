dd1e82181eeb4e4c9257ed5d1e6d10541d4d7ed6
vocalise.com <vocalise.com@gmail.com>
2024-02-21 10:04:56 +0800

    修改切換tab的邏輯

--- ToDoList Project.js
+++ ToDoList Project.js
@@ -1,174 +1,146 @@
-
-let Data = []
 const inputTxt = document.querySelector('.inputTxt')
-const AddBtn = document.querySelector('.btn_add')
-const List = document.querySelector('.list')
-const ListDelete = document.querySelector('.delete')
-const listFooter = document.querySelector('.list_footer')
-const deleteDoneItem = document.querySelector('.deleteDoneItem')
-const checkbox = document.querySelector('.checkbox')
-const tab =document.querySelector('.tab')
-// 陣列:儲存代辦事項狀態的
-const checkboxObj = document.querySelectorAll('.checkboxObj')
+const Add = document.querySelector('.btn_add')
+const list_content = document.querySelector('.list_content')
+const list_footer = document.querySelector('.list_footer')
+const tab = document.querySelector('.tab')
+// 儲存tab標籤
+const tabItem = document.querySelectorAll('.tab li')
+let Data =[]
+let filterData =[]
+let tabStatus = 'all'
 
-// 產生新的待辦事項清單
-function renderDataStr(){
-    let ListStr = ''
-    if(Data.length == 0){
-        ListStr = `<li>
-                        <label class="checkbox">
-                        <input class = "checkboxObj" id="" type="checkbox" />
-                            <span>請輸入待辦事項</span>
-                        </label>
-                    </li>`
-        List.innerHTML = ListStr
-        renderFooter()
+// 資料初始化
+function renderData(d){
+    ListStr = ''
+    if(d.length ==0){
+    list_content.innerHTML = ListStr
     }else{
-        Data.forEach(function(item,index){
-        ListStr += `<li>
-                            <label class="checkbox">
-                            <input class = "checkboxObj" id=${index} type="checkbox" ${item.checked}/>
-                                <span >${item.content}</span>
-                            </label>
-                            <a href="#" class="delete" data-index=${index}></a>
-                        </li>`
-        List.innerHTML = ListStr
-        // 計算待完成數量
-        renderFooter()
-        })
-    }
-
-}
-
-// 產生新的footer資料
-function renderFooter(){
-    // // 更改footer數量
-    let footerCount = Data.filter(function(item){
-        return item.checked ==''
+    d.forEach(function(item,index){
+    ListStr += `<li >
+                    <label class="checkbox">
+                        <input class="check" id=${index} type="checkbox" ${item.checked} />
+                        <span>${item.content}</span>
+                    </label>
+                    <a href="#" class="delete" data-index=${index}></a>
+                </li>`
+    list_content.innerHTML = ListStr
     })
-
-    // 將Footer數量寫回html
-    footerStr = `<p>${footerCount.length} 個待完成項目</p>
-                        <a class="deleteDoneItem" href="#">清除已完成項目</a>`
-    listFooter.innerHTML = footerStr
+    }
 }
 
+// Footer資料渲染
+function renderFooter(d){
+    let toDoItem = []
+    let footerStr = ''
+    toDoItem = d.filter(function(item){
+        return item.checked == ''
+    })
+    footerStr =`<p>${toDoItem.length}個待完成項目</p><a class="deleteDoneItem" href="#">清除已完成項目</a>`
+    list_footer.innerHTML = footerStr
+}
 
+// 更新待辦事項
+// 增加tab資料篩選條件判斷
+function updateData(updateData){
+    if(tabStatus =='itemToDo'){
+        updateData = Data.filter(function(item){
+            return item.checked ==""
+        })
+        renderData(updateData)
+        renderFooter(updateData)
+    }
+    else if (tabStatus =='itemDone'){
+        updateData = Data.filter(function(item){
+            return item.checked =="checked"
+        })
+        renderData(updateData)
+    }
+    else if(tabStatus =='all'){
+        updateData = Data
+        renderData(updateData)
+        renderFooter(updateData)
+    }
+}
 
-// 新增待辦事項
-AddBtn.addEventListener("click",function(e){
-    if (inputTxt.value == ''){
+// 新增待辦項目
+Add.addEventListener("click",function(e){
+    if(inputTxt.value =='' ){
         return alert('請輸入待辦事項')
+    }else{
+    
+    let dataObj = {
+        "content": inputTxt.value,
+        "checked": ''
     }
-    Data.push(
-        {
-            'content': `${inputTxt.value}`,
-            'checked': false
-        }
-        )
-    renderDataStr()
-    inputTxt.value = ''
+    Data.push(dataObj)
+    inputTxt.value =''
+
+    if(tabStatus != "all"){
+        updateData(Data)
+
+    }else{
+    updateData(Data)
+    }
+    
+    }
 })
 
-// 點擊待辦事項
-// 1.刪除待辦事項 
-// 2.分類待辦事項
-List.addEventListener("click",function(e){
-    if(Data.length == 0){
-        alert("請輸入待辦事項")
-        return
+list_content.addEventListener("click",function(e){
+    //  刪除選取項目
+    if (e.target.getAttribute('class') =="delete"){
+        let dataIndex = e.target.getAttribute('data-index')
+        Data.splice(dataIndex,1)
+        updateData(Data)
     }
-    
-    // 分類待辦事項
-    let checkboxObj = document.querySelectorAll('.checkboxObj')
-    if(Data.length!=0){
-    checkboxObj.forEach(function(item,index){
-    if(checkboxObj[index].checked == true){
-            Data[index].checked = 'checked'
-    }
-    if (checkboxObj[index].checked == false){
-            Data[index].checked =''
-        }
-    })
-    }
-    
-    renderFooter()
 
-    // // 點擊Tab
-    // // 當點擊到標籤時，刷新list資訊
-    tab.addEventListener('click',function(e){
-        let ListStr=''
-        let tabStr =''
-        Data.forEach(function(item,index){
-            if(e.target.getAttribute('id')=='itemToDo' && item.checked ==''){
-                ListStr += `<li>
-                            <label class="checkbox">
-                            <input class = "checkboxObj" id=${index} type="checkbox" ${item.checked} />
-                                <span >${item.content}</span>
-                            </label>
-                            <a href="#" class="delete" data-index=${index}></a>
-                        </li>`
-                List.innerHTML =ListStr
-                // tab hover
-                tabStr = `<li  id="all">全部</li>
-                <li class="active" id="itemToDo">待完成</li>
-                <li id="itemDone">已完成</li>`
-                tab.innerHTML =tabStr
-
+    //  變換checked狀態
+    else if(e.target.getAttribute('class')=="check"){
+        let CheckItem = document.querySelectorAll('.check')
+        let toDoItem = []
+        if(CheckItem.length!=0){
+            Data.forEach(function(item,index){
+            if(CheckItem[index].checked == true){
+                item.checked ='checked'
+            }else{
+                item.checked =''
             }
-            if(e.target.getAttribute('id')=='itemDone' && item.checked =='checked'){
-                ListStr += `<li>
-                            <label class="checkbox">
-                            <input class = "checkboxObj" id=${index} type="checkbox" ${item.checked} />
-                                <span >${item.content}</span>
-                            </label>
-                            <a href="#" class="delete" data-index=${index}></a>
-                        </li>`
-                List.innerHTML =ListStr
-                // tab hover
-                tabStr = `<li  id="all">全部</li>
-                <li  id="itemToDo">待完成</li>
-                <li class="active" id="itemDone">已完成</li>`
-                tab.innerHTML =tabStr
-            }
-            if(e.target.getAttribute('id')=='all'){
-                ListStr += `<li>
-                            <label class="checkbox">
-                            <input class = "checkboxObj" id=${index} type="checkbox" ${item.checked} />
-                                <span >${item.content}</span>
-                            </label>
-                            <a href="#" class="delete" data-index=${index}></a>
-                        </li>`
-                List.innerHTML =ListStr
-                // tab hover
-                tabStr = `<li class="active"  id="all">全部</li>
-                <li  id="itemToDo">待完成</li>
-                <li  id="itemDone">已完成</li>`
-                tab.innerHTML =tabStr
-            }
-            
         })
-    })
-    
-    // 刪除個別待辦事項
-    if(e.target.getAttribute("class")=="delete"){
-        let deleteIndex = e.target.getAttribute("data-index")
-        Data.splice(deleteIndex,1)
-        renderDataStr()
+        }
+        // footerCount
+        toDoItem = Data.filter(function(item){
+            return item.checked ==''
+        })
+        renderFooter(toDoItem)
     }
+})
 
-    // 刪除所有已完成項目
-    listFooter.addEventListener('click',function(e){
+// 清除已完成項目
+list_footer.addEventListener("click",function(e){
     if(e.target.getAttribute("class")=='deleteDoneItem'){
-        Data.forEach(function(item,index){
-            if(item.checked =='checked'){
-                Data.splice(index,1)
-            }
-            renderDataStr()
-        })
-        
+        Data = Data.filter(item => item.checked=='')
+        if(tabStatus =="all" || tabStatus == "itemToDo"){
+            updateData(Data)
+            renderFooter(Data)
+        }
+        else if(tabStatus == "itemDone"){
+            let cleanData = []
+            updateData(Data)
+        }
     }
+    
 })
 
+// 切換Tab
+// 把hover效果寫成函式
+tab.addEventListener("click",function(e){
+    tabStatus = e.target.getAttribute('id')
+    // tab hover 效果
+    // 1. 移除原本的active     2. 點到的項目加上active
+    tabItem.forEach(function(item,index){
+            item.classList.remove("active")
+            if(tabStatus == item.id){
+                item.classList.add("active")
+            }
+        })
+    updateData(Data)
 })
-
